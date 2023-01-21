70be8006fc253e155fe22c8671b3e7ba481c91bd
zohreh moazezi <zohre.m548@gmail.com>
2023-01-21 15:01:01 +0330

    friendss

---
+++ script.js
@@ -0,0 +1,30 @@
+// Difficulty: 4
+
+// async await
+
+
+// Named asynchronous function
+async function getUsers() {
+    let people = await fetch("https://randomuser.me/api/?results=5");
+    let data = await people.json();
+    const timeline = document.querySelector("#timeline");
+    data.results.forEach((person) => {
+      let image = document.createElement("img");
+      image.src = person.picture.medium;
+      timeline.appendChild(image);
+    });
+  }
+  
+  // Anonymous asynchronous function
+  // const getUsers = async () => {
+  //   let people = await fetch("https://randomuser.me/api/?results=5");
+  //   let data = await people.json();
+  //   const timeline = document.querySelector("#timeline");
+  //   data.results.forEach(person => {
+  //     let image = document.createElement("img");
+  //     image.src = person.picture.medium;
+  //     timeline.appendChild(image);
+  //   });
+  // };
+
+  getUsers();
